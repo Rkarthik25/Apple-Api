19b1eb8ea695118dcc3105aee97f0108cf733082
Rkarthik25 <karthijan111@gmail.com>
2023-02-25 01:01:24 +0530

    add

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const { connection } = require("mongoose")
 const db=require("./db")
 const app=express()
 
-const{productRouter}=require("./route/ProductRoute")
+const{ productRouter}=require("./route/ProductRoute")
 app.use(express.json())
 
 app.get("/",(req,res)=>{
     res.send("home page")
 })
-
 app.use("/product",productRouter)
+
 app.listen(1727,async()=>{
     try {
         await connection
         console.log("connected to db")
-
+        
     } catch (err) {
         console.log("not connected")
     }
